bbfbe43bf679371fa701635ce0986d84b87810d8
Kelly-Ruiz <52898090+Kelly-Ruiz@users.noreply.github.com>
2024-05-15 17:35:11 -0500

    Update
    Cambio del codigo

--- Videos/Reproductos Basico de JS.js
+++ Videos/Reproductos Basico de JS.js
@@ -1,3 +1,9 @@
-const video= document.getElementsByTagName("video")[0]
-//ACTIVAR CONTROLES DE JS
-video.controls = "controls";
+const video = document.getElementsByTagName("video")
+for (i = 0; i < video.length ; i++){
+  //validar todos los videos
+	if(!video[i].paused){
+    //activar controles en el video que se este reproduciendo
+		video[i].controls = "controls";
+		break
+	}
+}
